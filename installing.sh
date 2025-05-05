c082ca9423abd905f2be3911c2ad1fc66b1981bc
idurgabhavani <inukondadurgabhavani426@gmail.com>
2025-05-05 16:21:14 +0530

    first script

--- installing.sh
+++ installing.sh
@@ -20,3 +20,13 @@ else
     echo "installing is success"
 fi
 
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "instaling my GIT is failed "
+    exit 1
+else
+    echo "installing is success"
+fi
+
