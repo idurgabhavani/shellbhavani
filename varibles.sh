be2a9edc66a963e57b3bd32f08fc0fc656474984
idurgabhavani <inukondadurgabhavani426@gmail.com>
2025-05-05 15:30:51 +0530

    first script

--- varibles.sh
+++ varibles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo " hi teja"
-echo " hi bhavani"
-echo "how are you"
-echo " fine how are you"
-echo " everthing goinh well"
+username=$1
+userpass=$2
+
+echo " username name is : $username , password is : $userpass "
+
