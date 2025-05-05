cca43e9e7a48fa901261efe24f6a087099bdcb75
idurgabhavani <inukondadurgabhavani426@gmail.com>
2025-05-05 15:17:44 +0530

    first script

--- varibles.sh
+++ varibles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-eacho " hi teja"
-eacho " hi bhavani"
-eacho "how are you"
-eacho " fine how are you"
-eacho " everthing goinh well"
+echo " hi teja"
+echo " hi bhavani"
+echo "how are you"
+echo " fine how are you"
+echo " everthing goinh well"
