a2e463b341c9e224bd7296582aebb1ae194d212a
idurgabhavani <inukondadurgabhavani426@gmail.com>
2025-05-07 21:32:37 +0530

    new

--- logs.sh
+++ logs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 
+LOGFILE="tmp/$0-$TIMESTAMP.log"
 echo "script name : $0 "
 
 
@@ -24,11 +26,11 @@ else
     echo " you are root user"
 fi
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? "installing MYSQL"
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 VALIDATE $? "installing GIT"
 
