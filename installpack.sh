00ad7b3cfef8b7a21e28736b917cbb2799136699
idurgabhavani <inukondadurgabhavani426@gmail.com>
2025-05-12 16:30:20 +0530

    new

--- installpack.sh
+++ installpack.sh
@@ -2,12 +2,37 @@
 
 ID=$(id -u)
 
+TIMESTAMPE=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMPE.log"
+
+echo "script   started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [$1 -ne 0] 
+    then
+        echo -e "FAIELD"
+    else
+        echo -e "SUCCESS"
+
+}
+
 if [ $ID ne 0 ]
 then
-    echo -e "$R ERROR: please run this script with root access 4N"
+    echo -e " ERROR: please run this script with root access 4N"
     exit 1
 else
     echo "you are not root  user"
 fi
 
-echo " all argumaents passed : $@ "
+#echo " all argumaents passed : $@ "
+
+for package in $@
+do
+    yum list installed $package
+    if [$? -ne 0]
+    then
+        yum insall $package -y
+        VALIDATE $? "installation of $package "
+    else
+        echo -e "$package is already INSALLED .. $Y skipping"
+done
