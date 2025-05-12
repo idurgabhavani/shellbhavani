37448f3b1d9c023c0fb40ae7be693d4338ae492c
idurgabhavani <inukondadurgabhavani426@gmail.com>
2025-05-12 16:14:57 +0530

    new

--- installpack.sh
+++ installpack.sh
@@ -7,7 +7,7 @@ then
     echo -e "$R ERROR: please run this script with root access 4N"
     exit 1
 else
-    echo "you are not user"
+    echo "you are not root  user"
 fi
 
 echo " all argumaents passed : $@ "
