07dd12b9da4660a30ecf2e6b9b49981b6212bcb8
idurgabhavani <inukondadurgabhavani426@gmail.com>
2025-07-15 19:59:26 +0530

    new

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 AMI=ami-0150ccaf51ab55a51 # THESE WE WILL GET FROM INSTANCE
 SG_ID=sg-07f6c69ef91259553 # SECIROTU GROUP ID
-INSTANCE=("mongodb" "redis" "mysql" "rabitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE=("mongodb" "redis" "mysql" "rabitmq" "catalogue" "user") #"cart" "shipping" "payment" "dispatch" "web")
 
 for i in "${INSTANCE[@]}"
 do
@@ -14,6 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --key-name bhavani --security-group-ids $SG_ID --key-name bhavani --security-group-ids sg-07f6c69ef91259553 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --key-name bhavani --security-group-ids $SG_ID --key-name bhavani --security-group-ids sg-07f6c69ef91259553 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i= $IP_ADDRESS"
 done
